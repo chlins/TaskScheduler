478fefc8e9b77aa3a4678fc4fc542717e68fc7f5
zcytop <zcytop@gmail.com>
2017-09-14 22:14:34 +0800

    Optimize some code of job

--- job/job.go
+++ job/job.go
@@ -5,3 +5,21 @@ type Jober interface {
 	//Start jobs
 	Start()
 }
+
+//JobFunc common func
+type JobFunc func()
+
+//Job is a common struct
+type Job struct {
+	jobFunc JobFunc
+}
+
+//NewJob is produce a job
+func NewJob(job JobFunc) *Job {
+	return &Job{jobFunc: job}
+}
+
+//Start of job
+func (j *Job) Start() {
+	go j.jobFunc()
+}
--- main.go
+++ main.go
@@ -1,22 +1,17 @@
 package main
 
 import (
+	"TaskScheduler/job"
 	"TaskScheduler/scheduler"
 	"TaskScheduler/trigger"
 	"log"
 	"time"
 )
 
-type job struct{}
-
-func (j job) Start() {
-	log.Println("Hello World")
-}
-
 func main() {
-	trigger := trigger.NewTrigger(time.Now().Add(30*time.Second), 5, "second")
-	job1 := job{}
-	job2 := job{}
-	scheduler := scheduler.NewScheduler(trigger, job1, job2)
+	trigger := trigger.NewTrigger(time.Now().Add(10*time.Second), 5, "second")
+	j1 := job.NewJob(func() { log.Println("I am job 1...") })
+	j2 := job.NewJob(func() { log.Println("I am job 2...") })
+	scheduler := scheduler.NewScheduler(trigger, j1, j2)
 	scheduler.Start()
 }
